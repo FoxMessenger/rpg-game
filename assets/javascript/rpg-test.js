3e696fbc01ecd11a1d1a3627145dd8811927a4f5
FoxMessenger <liamfox@dhcp-10-105-154-238.wireless.northwestern.private>
2017-02-22 18:48:31 -0600

    Game is fully working

--- assets/javascript/rpg-test.js
+++ assets/javascript/rpg-test.js
@@ -35,34 +35,19 @@ $(document).ready(function() {
 	var playerChamp;					// YOUR character - this will take the champAvailable function and set it as player - then has that value set to null so the it's not reselectable
 	var playerHP;
 	var playerCurrentHP
-
 	var enemyChamp;						// once your character is chosen now it's the choosing opponent phase - this will take the champAvailable function, minus the var Player choice
 	var enemyHP;
 	var enemyCurrentHP;
-
 	var champLocked = false;			// locking your champ in
 	var enemyLocked = false;			// same as above, but for the opponent
-
-	var gameState;						// create an object that stores information about the game state - this will be the state of continues, play again, game over, etc.
-
-	var playerDamage = false;
-	var enemyDamage = false;
 	var combat = false;
-	
 	var name;							// champ name
 	var hitPoints;						// champ HP
 	var attPower;						// champ Attack power
 	var armor;							// champ armor
-
 	var victory;						// if you win 1 match
 	var wins;							// if you win 5 matches
 	var defeat;							// if you lose 1 match
-
-	var attack = function(){};			// function attack button ---> the game asks that the attackPower increases every time you attack.
-										// var attackPower
-	var counterAtt = function(){};		// -- extra, random counter attack function -- //
-
-	var play = function(){};			// function to start game
 
 
 		// --- A function that contains the specific stats of the champ --- //
@@ -72,6 +57,7 @@ $(document).ready(function() {
 		this.hitPoints = hitPoints;
 		this.attPower = attPower;
 		this.icon = icon;
+		this.hit = hit;
 		//this.armor: armor;			// if time allows I'll add this variable
 	}; // -- END ChampStats 
 
@@ -84,14 +70,14 @@ $(document).ready(function() {
 		new champStats('Super Girl', 3300, 110, '<img src="assets/images/1678.png">', '<img src="assets/images/1678-hit.png">'), 
 		new champStats('Aphrodite', 4000, 165, '<img src="assets/images/2534.png">'/*, 40 */), 
 		new champStats('Sasha', 3500, 120, '<img src="assets/images/2372.png">'/*, 15 */), 
-		new champStats('Artemis', 4200, 220, '<img src="assets/images/3274.png">'/*, 12 */), 
-		new champStats('Hades', 3800, 155, '<img src="assets/images/1949.png">'/*, 28 */), 
-		new champStats('Sephiroth', 3200, 200, '<img src="assets/images/2032.png">'/*, 6 */), 
-		new champStats('Ares', 3650, 195, '<img src="assets/images/2081.png">'/*, 30 */), 
-		new champStats('Athena', 3500, 215, '<img src="assets/images/3285.png">' /*, 6 */), 
-		new champStats('Rei', 4200, 175, '<img src="assets/images/3393.png">' /*, 52 */), 
-		new champStats('Asuka', 3600, 185, '<img src="assets/images/3396.png">' /*, 13 */), 
-		new champStats('Zues', 5000, 95, '<img src="assets/images/851.png">' /*, 70 */)
+		// new champStats('Artemis', 4200, 220, '<img src="assets/images/3274.png">'/*, 12 */), 
+		// new champStats('Hades', 3800, 155, '<img src="assets/images/1949.png">'/*, 28 */), 
+		// new champStats('Sephiroth', 3200, 200, '<img src="assets/images/2032.png">'/*, 6 */), 
+		// new champStats('Ares', 3650, 195, '<img src="assets/images/2081.png">'/*, 30 */), 
+		// new champStats('Athena', 3500, 215, '<img src="assets/images/3285.png">' /*, 6 */), 
+		// new champStats('Rei', 4200, 175, '<img src="assets/images/3393.png">' /*, 52 */), 
+		// new champStats('Asuka', 3600, 185, '<img src="assets/images/3396.png">' /*, 13 */), 
+		// new champStats('Zues', 5000, 95, '<img src="assets/images/851.png">' /*, 70 */)
 	]; // -- END champions
 
 
@@ -119,10 +105,10 @@ $(document).ready(function() {
 	$('.champ').on('click', function(event){										// adding the champ to the selected champ section (this section is different from the space with all the champs)
 			
 		if (champLocked === false) {
-			var champSelection = $('<div>');										// deciding that I will make the section a new Div
+			champSelection = $('<div>');										// deciding that I will make the section a new Div
 			$('.champFader').not(this).removeClass('champFader');
 			$(this).addClass('champFader');											// adding an ID can only be added to 1 element, so the last element you picked will have this ID and no other
-			var champInfo = $(this);												// champInfo is now equal to this (which is champAvailable I believe). ChampInfo now can have object attributes of .name .hp and .attack, because I named them earlier with data-hp, data-name, etc. 
+			champInfo = $(this);												// champInfo is now equal to this (which is champAvailable I believe). ChampInfo now can have object attributes of .name .hp and .attack, because I named them earlier with data-hp, data-name, etc. 
    			var data = {
        			name: champInfo.attr('id'),
         		hp: champInfo.data('hp'),
@@ -145,9 +131,9 @@ $(document).ready(function() {
 			
 			$('#confirmChamp').fadeIn( 500 );																// fades in confirmChamp Button from champion onclick
 			$('#arenaChamp').html(																			// this will take my #arenaChamp space and port the following data information 	
-				`<p>${data.name}</p>
-        		 <p>HP ${data.hp}</p>
-        		 <p>attack ${data.attack}</p>`																						
+			`	<p>${data.name}</p>
+        		<p>HP ${data.hp}</p>
+        		<p>attack ${data.attack}</p>	`																						
     			); // -- END data collection
 
 			// -- confirming your Champion --//
@@ -162,7 +148,7 @@ $(document).ready(function() {
 				} // -- END champLocked statemenet
 
 				$('#chooseChamp').addClass('chooseEnemy');
-				$('#chooseChamp').html('CHOOSE YOUR FOE');
+				$('#chooseChamp').html('CHOOSE AN ENEMY');
 				$(this).fadeOut( 400 );													// fades in confirmChamp Button from champion onclick
 				console.log("You've Selected " + playerChamp.name);
 			}); // End confirm champ
@@ -181,7 +167,7 @@ $(document).ready(function() {
 			var champSelection = $('<div>');										// deciding that I will make the section a new Div
 			$('.enemyFader').not(this).removeClass('enemyFader');
 			$(this).addClass('enemyFader');												// adding an ID can only be added to 1 element, so the last element you picked will have this ID and no other
-			var champInfo = $(this);												// champInfo is now equal to this (which is champAvailable I believe). ChampInfo now can have object attributes of .name .hp and .attack, because I named them earlier with data-hp, data-name, etc. 
+			champInfo = $(this);												// champInfo is now equal to this (which is champAvailable I believe). ChampInfo now can have object attributes of .name .hp and .attack, because I named them earlier with data-hp, data-name, etc. 
    			var data = {
        			name: champInfo.attr('id'),
         		hp: champInfo.data('hp'),
@@ -201,13 +187,11 @@ $(document).ready(function() {
 			
 			
 			$('#enemyChamp').html(champSelection);															// this ports the cloned image to the html
-			
 			$('#confirmEnemy').fadeIn( 400 );																// fades in confirmChamp Button from champion onclick
-
 			$('#arenaEnemy').html(																			// this will take my #arenaChamp space and port the following data information 	
-				`<p>${data.name}</p>
-        		 <p>HP ${data.hp}</p>
-        		 <p>attack ${data.attack}</p>`																						
+			`	<p>${data.name}</p>
+        		<p>HP ${data.hp}</p>
+        		<p>attack ${data.attack}</p>	`
     			); // -- END data collection
 			
 
@@ -220,15 +204,15 @@ $(document).ready(function() {
 					$('.enemyFader').remove();
 					$(this).hide( );														// fades in confirmChamp Button from champion onclick
 					$("#attBtn").fadeIn( 500 );
-			
 					console.log('Your Opponent is ' + enemyChamp.name);
-				
 					$('#chooseChamp').hide();
 
 					combat = true;
 					console.log( 'Prepare for Combat!' );
 				} // -- END enemyLocked statement
+
 			}); // End confirm Enemy
+
     	} //-- END champLocked if statement
 
 	}); // -- END Opponent Select Space -- //
@@ -236,101 +220,91 @@ $(document).ready(function() {
 
 	// --- COMBAT --- //
 
- 	$('#attBtn').on('click', function(){
+ 	$('#attBtn').on('click', function(event){
+
  		$('#damageUpdate').show();
- 		var timeoutID;
 
-		// $(this).find('/image/1678-hit.png').toggle();
+		// $(this).('/image/1678-hit.png').toggle();
 
- 		if ( !combat ) {
- 			console.log('Please choose an opponent')
+ 		if ( !combat && playerChamp.hp > 0 ) { // if combat is false, have them choose another champ
+
+ 			$('#damageUpdate').html('Choose another opponent!');
+
+ 		} else if ( combat === true && playerChamp.hp <= 0) { // if combat is false AND player HP is <= 0, console.log you're dead
+ 			
+ 			$('#damageUpdate').html("You're Dead");
+ 			combat = false;
+ 			enemyLocked = false;
+
  		} else if ( combat === true ){
+ 			
  			if (playerChamp.attack < enemyChamp.hp) {
- 				if (enemyChamp.attack > playerChamp.hp) {
- 					
- 				}
+					
 					enemyChamp.hp -= playerChamp.attack;
-					$('#damageUpdate').prepend("You hit for " + playerChamp.attack + "<p>" + "<hr>");
-
+					$('#damageUpdate').html("You hit for " + playerChamp.attack + "<p>" + "<hr>");
 					playerChamp.hp -= enemyChamp.attack;
 
 					$(arenaEnemy).html(
-						`<p>${enemyChamp.name}</p>
-        		 		 <p>HP ${enemyChamp.hp}</p>
-        		 		 <p>attack ${enemyChamp.attack}</p>`																						
-    				);
+					`	<p>${enemyChamp.name}</p>
+        		 		<p>HP ${enemyChamp.hp}</p>
+        		 		<p>attack ${enemyChamp.attack}</p>	`
+    				); // --- END Arena ENEMY Update
 					
 					$(arenaChamp).html(
-						`<p>${playerChamp.name}</p>
-        		 		 <p>HP ${playerChamp.hp}</p>
-        		 		 <p>attack ${playerChamp.attack}</p>`																						
-    				);
+					`	<p>${playerChamp.name}</p>
+        		 		<p>HP ${playerChamp.hp}</p>
+        		 		<p>attack ${playerChamp.attack}</p>	`																						
+    				); // --- END Arena PLAYER update
 
-					console.log("Your HP: " + playerChamp.hp);
-					playerChamp.attack += 25;
-							$('#damageUpdate').show();
-					$('#damageUpdate').prepend(enemyChamp.name + " Hit back for: " + enemyChamp.attack + "<p>" +  "<hr>");
+					playerChamp.attack += 50;
+					$('#damageUpdate').show();
+					$('#damageUpdate').html(enemyChamp.name + " Hit back for: " + enemyChamp.attack + "<p>" +  "<hr>");
 
-			 } else if (playerChamp.attack === enemyChamp.hp || playerChamp.attack > enemyChamp.hp) {
-					playerChamp.attack += 25;
-					$('#damageUpdate').html('Victory! ');
+			} //--- END Champ.attack < Enemy.hp
+			  else if (enemyChamp.attack > playerChamp.hp || enemyChamp.attack === playerChamp.hp) {
+	
+ 					console.log("you died!")
+ 					$('#damageUpdate').html("You Died...");
+					enemyLocked = false;
+					combat = false;
+
+			} else if (playerChamp.attack === enemyChamp.hp || playerChamp.attack > enemyChamp.hp) {
+					
+					enemyChamp.hp = 0;
+					$('#arenaEnemy').html(
+					`	<p>${enemyChamp.name}</p>
+        		 		<p>HP ${enemyChamp.hp}</p>
+        		 		<p>attack ${enemyChamp.attack}</p>	`
+    				); // --- END Arena PLAYER update
+
+					playerChamp.attack += 105;
+					$('#damageUpdate').html( 'Victory! '	);
 					wins += 1;
 
-					$('#damageUpdate').append("Wins: " + wins);
+					$("#attBtn").hide();
+
+					$('#damageUpdate').append("Wins: " + wins + "." + " Choose a new Opponent");
 					combat = false;
+
 					if (wins === 3) {
-						console.log ("You Win!")
-					} else { 
+						$('#damageUpdate').html('You Win!'	);
+					} //--- END WIN statement
+					  
+					  else { //restart enemy selection and combat
 						enemyLocked = false;
 						combat = false;
-					}
-			}
-					// end attack combat
+					} // --- END else, restart combat
+
+			} else if (enemyChamp.attack > playerChamp.hp || enemyChamp.attack === playerChamp.hp ) { 
+			
+			} // end attack combat
  		} // -- END Combat
 	 }); // -- END onClick
 	
 	wins = 0;
+
+	// var reset.on('click', function(){} 
 }); // -- End Code
-
-
-
-
-	// var currentPlayerHealth = player.health;
-	// var currentEnemyHealth = enemy.health;
-	
-	// var combat = function(){			// the combat logic
-	// 	combat = true;
-	// };
-
-		// Combat Pseudo Code: champ vs opponent
-		
-		// $('#attBtn').on('click', function(event) {
-		// 	
-		// 	} 
-		// if (wins === 3) {
-					// in the area display 'You Win!'
-		//  }
-
-		// 	// run function enemy attack phase
-
-		// 	if (enemy.attPower < player.hitPoints) {
-		// 			enemy.hitPoints -= player.attPower;
-		// 			// end attack combat
-		// 	}  if else (enemy.attPower === player.hitPoints || enemy.attPower > player.hitPoints) {
-		// 			// end combat
-					// in the arena display "Defeat"
-		//			// in the area display "Continue?"
-					// if (continue === true) {
-					// 	continue -= 1;
-					// } else {
-					// 	end game
-					// }
-					// display "Play Again" in arena
-		// 	} 		
-		// };
-
-
-
 
 
 
